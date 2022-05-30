a18360f1ae78697694a64c6fa5d15dd446076ec8
DESKTOP-D5NHP53\Maaz Javaid <mmaazj323@gmail.com>
2022-05-30 07:48:19 +0500

    array list and adapter created

--- app/src/main/java/com/example/adaptersandarraylist/MainActivity.java
+++ app/src/main/java/com/example/adaptersandarraylist/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.adaptersandarraylist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ArrayList<String> studentList = new ArrayList<String> ();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView listview=findViewById(R.id.listview);
+        studentList.add("Saad");
+        studentList.add("Zahid");
+        studentList.add("Saeed");
+        studentList.add("Zainab");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1, studentList);
+        listview.setAdapter(arrayAdapter);
+
     }
 }
